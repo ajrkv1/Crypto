e5b0851f0257b9fecd4add16f461beb1d849beb1
ajrkv1 <shaharma03@gmail.com>
2021-02-02 23:34:20 +0200

    Finished keccakf permutation function

--- sha256.c
+++ sha256.c
@@ -1,49 +1,52 @@
 #include <stdlib.h>
+#include <stdio.h>
 
+#define STATE_SIZE 5
+#define ROUNDS 24
 
 unsigned long rot(unsigned long num, int rotate ){
-	return (num << rotate)|(num >> sizeof(num)-rotate);
+	return (num << rotate)|(num >> (sizeof(num)-rotate));
 }
 
-unsigned long ** keccakfround(unsigned long  a[][5], unsigned long r){
+unsigned long ** keccakfround(unsigned long ** a, unsigned long r){
 	
 	//theta step
 	
 	//xor parity bits
-	unsigned long parity[5];
-	for(int i=0;i<5;i++){
+	unsigned long parity[STATE_SIZE];
+	for(int i=0;i<STATE_SIZE;i++){
 		unsigned long par = a[i][0]; 
-		for(int j=1;j<5;j++){
+		for(int j=1;j<STATE_SIZE;j++){
 			par ^= a[i][j];
 		}
 		parity[i] = par;
 	}
 	
 	//alter a correctly
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			a[i][j] = a[i][j] ^ (parity[(i-1)%5] ^ rot(parity[(i+1)%5],1));
+	for(int i=0;i<STATE_SIZE;i++){
+		for(int j=0;j<STATE_SIZE;j++){
+			a[i][j] = a[i][j] ^ (parity[(i-1)%STATE_SIZE] ^ rot(parity[(i+1)%STATE_SIZE],1));
 		}
 	}
 
 	//ro and pi and chi steps
 	
-	unsigned long rot_offsets[5][5] = {{ 0, 1,62,28,27},
+	unsigned long rot_offsets[STATE_SIZE][STATE_SIZE] = {{ 0, 1,62,28,27},
 					 {36,44, 6,55,20},
 					 { 3,10,43,25,39},
 					 {41,45,15,21, 8},
 					 {18, 2,61,56,14}};
 	//temp array
-	unsigned long b[5][5];
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			b[j][(2*i+3*j)%5] = rot(a[i][j],rot_offsets[i][j]); 
+	unsigned long b[STATE_SIZE][STATE_SIZE];
+	for(int i=0;i<STATE_SIZE;i++){
+		for(int j=0;j<STATE_SIZE;j++){
+			b[j][(2*i+3*j)%STATE_SIZE] = rot(a[i][j],rot_offsets[i][j]); 
 		}
 	}
 
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			a[i][j] = b[i][j] ^ ((~b[(i+1)%5][j]) & b[(i+2)%5][j]);
+	for(int i=0;i<STATE_SIZE;i++){
+		for(int j=0;j<STATE_SIZE;j++){
+			a[i][j] = b[i][j] ^ ((~b[(i+1)%STATE_SIZE][j]) & b[(i+2)%STATE_SIZE][j]);
 		}
 	}
 
@@ -75,29 +78,30 @@ unsigned long ** keccakfround(unsigned long  a[][5], unsigned long r){
 				      0x8000000080008008};
 
 	a[0][0] = a[0][0] ^ round_consts[r];
-	
+	return a;	
 }
 
 
 
 unsigned long * keccakf(unsigned long * bl ){
-	
-	unsigned long ** state = malloc(25 * sizeof(unsigned long));
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			state[i][j] = *(bl+i+j);
+
+	unsigned long ** state = malloc(STATE_SIZE * sizeof(unsigned long *));
+	for(int i=0;i<STATE_SIZE;i++){
+		state[i] = malloc(STATE_SIZE * sizeof(unsigned long));
+		for(int j=0;j<STATE_SIZE;j++){
+			state[i][j] = *(bl+(i*STATE_SIZE)+j);
 		}
 	}
-	
-	for(int i=0;i<24;i++){
+
+	for(int i=0;i<ROUNDS;i++){
 		state = keccakfround(state, i);
 	}
 	
-	unsigned long * res = malloc(25*sizeof(unsigned long));
+	unsigned long * res = malloc(STATE_SIZE * STATE_SIZE *sizeof(unsigned long));
 
 	for(int i=0;i<5;i++){
 		for(int j=0;j<5;j++){
-			*(res+i+j) = state[i][j];
+			*(res+(i * STATE_SIZE)+j) = state[i][j];
 		}
 	}
 
@@ -105,3 +109,6 @@ unsigned long * keccakf(unsigned long * bl ){
 
 }
 
+int main(){
+return 0;
+}
